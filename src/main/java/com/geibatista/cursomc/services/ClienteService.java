a7d1789364be86aa424203a06c68dfe314724813
GeiBatista <geibatista@hotmail.com>
2021-09-01 22:08:41 -0300

    Ajustando e testando a deleção de clientes

--- src/main/java/com/geibatista/cursomc/services/ClienteService.java
+++ src/main/java/com/geibatista/cursomc/services/ClienteService.java
@@ -56,7 +56,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir um cliente que possui produtos!");
+			throw new DataIntegrityException("Não é possível excluir um cliente que possui pedidos!");
 		}
 	}
 	
