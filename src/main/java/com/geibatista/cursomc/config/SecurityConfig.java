563c2bc3da1783200b075d61cca0563222f1c6e0
GeiBatista <geibatista@hotmail.com>
2021-11-19 11:44:49 -0300

    Autorizando endpoints para perfis específicos

--- src/main/java/com/geibatista/cursomc/config/SecurityConfig.java
+++ src/main/java/com/geibatista/cursomc/config/SecurityConfig.java
@@ -44,7 +44,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
 			"/categorias/**",
-			"/clientes/**"
+			"/estados/**"
+	};
+	
+	private static final String[] PUBLIC_MATCHERS_POST = {
+			"/clientes/**",
+			"/auth/forgot/**"
 	};
 	
 	protected void configure(HttpSecurity http) throws Exception {	
@@ -54,7 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
-//			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
