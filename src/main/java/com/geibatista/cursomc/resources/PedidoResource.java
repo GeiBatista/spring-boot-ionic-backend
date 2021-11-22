b83f5ca57637e6b81884de7c5e239bf99cf6084e
GeiBatista <geibatista@hotmail.com>
2021-11-22 14:35:50 -0300

    Refresh token

--- src/main/java/com/geibatista/cursomc/resources/PedidoResource.java
+++ src/main/java/com/geibatista/cursomc/resources/PedidoResource.java
@@ -43,7 +43,7 @@ public class PedidoResource {
 	public ResponseEntity<Page<Pedido>> findPage(
 			@RequestParam(value="page", defaultValue="0") Integer page, 
 			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
-			@RequestParam(value="orderBy", defaultValue="instante") String orderBy, 
+			@RequestParam(value="orderBy", defaultValue="instant") String orderBy, 
 			@RequestParam(value="direction", defaultValue="DESC") String direction) {
 		Page<Pedido> list = service.findPage(page, linesPerPage, orderBy, direction);
 		return ResponseEntity.ok().body(list);
