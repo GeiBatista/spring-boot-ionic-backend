88cf389476faeeeaf56eb13f874f7ea2c998bd7d
GeiBatista <geibatista@hotmail.com>
2021-11-24 10:56:35 -0300

    ajustando tamanho da imagem

--- src/main/java/com/geibatista/cursomc/services/ClienteService.java
+++ src/main/java/com/geibatista/cursomc/services/ClienteService.java
@@ -45,8 +45,8 @@ public class ClienteService {
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
 	
-//	@Value("${img.profile.size}")
-//	private Integer size;
+	@Value("${img.profile.size}")
+	private Integer size;
 	
 	@Autowired
 	private ClienteRepository repo;
@@ -136,13 +136,11 @@ public class ClienteService {
 		UserSS user = UserService.authenticated();
 		if (user == null) {
 			throw new AuthorizationException("Acesso negado");
-		}
-		
+		}		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
-		String fileName = prefix + user.getId() + ".jpg";
-				
-//		jpgImage = imageService.cropSquare(jpgImage);
-//		jpgImage = imageService.resize(jpgImage, size);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		String fileName = prefix + user.getId() + ".jpg";				
 				
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
