f81e888575f00e570efd6d5756ae26ce7e39f4b4
GeiBatista <geibatista@hotmail.com>
2021-11-24 17:19:54 -0300

    Padronizando formato das exceções

--- src/main/java/com/geibatista/cursomc/resources/Exception/ValidationError.java
+++ src/main/java/com/geibatista/cursomc/resources/Exception/ValidationError.java
@@ -8,8 +8,10 @@ public class ValidationError extends StandardError {
 	
 	private List<FieldMessage> erros = new ArrayList<>();
 	
-	public ValidationError(Integer status, String msg, Long timeStamp) {
-		super(status, msg, timeStamp);
+	
+
+	public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
+		super(timestamp, status, error, message, path);
 	}
 
 	public List<FieldMessage> getErrors() {
